825d9b00e0d400846cc94d6105db5dae4d6b8ed0
venkat <pvsandeep21@gmail.com>
2024-04-10 16:13:40 -0500

    checking

--- mongo.sh
+++ mongo.sh
@@ -38,7 +38,7 @@ VALIDATE $? "enabled mongodb"
 systemctl start mongod &>> $LOGFILE
 VALIDATE $? "started mongo"
 
-sed -i '/s/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
 VALIDATE $? "changed the confg successfully"
 
 systemctl restart mongod
